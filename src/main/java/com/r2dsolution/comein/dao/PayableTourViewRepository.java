917259d93f641c9d12c2668da757c698137a2818
Jaroon <jaroon.k@gmail.com>
2022-11-20 15:13:30 +0700

    fix(dashboard):add tourId to tour pay dashboard

--- src/main/java/com/r2dsolution/comein/dao/PayableTourViewRepository.java
+++ src/main/java/com/r2dsolution/comein/dao/PayableTourViewRepository.java
@@ -12,8 +12,8 @@ public interface PayableTourViewRepository extends JpaSpecificationExecutor<Paya
 
 	List<PayableTourView> findByCompanyIdAndStatus(Long companyId, String status);
 	
-	@Query("select c.periodId as period_id, c.tourName as tour_name, c.note, sum(c.netValue) as total_net_value from PayableTourView c "
-			+ "where c.periodId = ?1 and c.status = ?2 group by c.periodId, c.tourName, c.note")
+	@Query("select c.periodId as period_id, c.tourId as tour_id, c.tourName as tour_name, c.note, sum(c.netValue) as total_net_value from PayableTourView c "
+			+ "where c.periodId = ?1 and c.status = ?2 group by c.periodId, c.tourId, c.tourName, c.note")
 	List<Object[]> sumPayableTourByPeriod(Long periodId, String status);
 	
 	List<PayableTourView> findByPeriodIdAndTourIdAndStatus(Long periodId, Long tourId, String status);
